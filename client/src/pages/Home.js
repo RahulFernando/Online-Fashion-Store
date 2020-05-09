af4e3c4afa269abe0ebaf6bd5ba5ea8e76678662
plhewa <plhewa2@gmail.com>
2020-05-09 15:30:04 +0530

    Display Item images in Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -26,17 +26,18 @@ export default class Home extends Component {
         this.addToCart = this.addToCart.bind(this);
     
         this.state = {
-            img: '',
             items: [],
             userId:''
         }
     }
 
     arrayBufferToBase64(buffer) {
+
+        var base64Flag = 'data:image/jpeg;base64,';
         var binary = '';
         var bytes = [].slice.call(new Uint8Array(buffer));
         bytes.forEach((b) => binary += String.fromCharCode(b));
-        return window.btoa(binary);
+        return base64Flag + window.btoa(binary);
     };
 
 
@@ -46,14 +47,13 @@ export default class Home extends Component {
         getItemDetails()
         
         .then(res => {
-            var base64Flag = 'data:image/jpeg;base64,';
-            var imageStr = this.arrayBufferToBase64(res.data.products[0].image.data.data);
+           
             this.setState({
                 
                 items: res.data.products,
-                img: base64Flag + imageStr
             })
 
+           
         })
         .catch (() => {
             alert('Error retreving data')
@@ -128,10 +128,14 @@ export default class Home extends Component {
                 
                 {this.state.items.map(product => {
                     return <div className="col-12 col-sm-6 col-md-4 col-lg-3" key={product._id}>
+                         
                     <div className="card card-block" style={useStyles.background}>
+                    <Link to={"/displayProduct/"+product._id}> 
                         <div className="overflow">
-                            <img src={this.state.img} alt="" className="card-img-top"/>
+
+                            <img src={this.arrayBufferToBase64(product.image.data.data)} alt="" className="card-img-top"/>
                         </div>
+                        </Link>
                         <div className="card-body text-dark">
                             <Link to={"/displayProduct/"+product._id}> <h5 className="card-title">{product.itemName}</h5> </Link>
                             <p className="card-text text-secondary">
