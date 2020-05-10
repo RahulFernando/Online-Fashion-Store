d4e8e2723d46e32e639dd186495f73a87eee455a
RahulFernando <rahulrulz680@gmail.com>
2020-05-10 12:23:55 +0530

    async function

--- server/route/cart.js
+++ server/route/cart.js
@@ -4,104 +4,109 @@ const router = express.Router();
 const User = require('../models/user'); //user schema
 
 //Add items to the Cart
-router.route('/add/:id').post((req,res) => {
-
-
-    User.findOne({ _id: req.params.id }, (err, userInfo) => {
-        let alreadyInCart = false;
-
-        userInfo.Cart.forEach((item) => {
-
-        
-            if (item.id == req.body.itemID) {
-                alreadyInCart = true;
-                
-            }
-        })
-
-        if (alreadyInCart) {
-
-
-        console.log("This item is already in the cart ")
-
-          User.findOneAndUpdate(
-                { _id: req.params.id, "Cart.id": req.body.itemID},
-                { $inc: { "Cart.$.quantity": 1 } },
-                { new: true },
-                () => {
-                    if (err) return res.json({ success: false, err });
-                    res.status(200).json(userInfo.addToCart)
-                }
-            )
-
-       
-        } else {
-
-            console.log("In the else clause")
+router.route('/add/:id').post(async (req,res) => {
+    try {
+        await User.findOne({ _id: req.params.id }, (err, userInfo) => {
+            let alreadyInCart = false;
+    
+            userInfo.Cart.forEach((item) => {
+    
             
-            User.findOneAndUpdate(
-                { _id: req.params.id },
-                {
-                    $push: {
-                        Cart: {
-                            id: req.body.itemID,
-                            quantity: 1
-                        }
+                if (item.id == req.body.itemID) {
+                    alreadyInCart = true;
+                    
+                }
+            })
+    
+            if (alreadyInCart) {
+    
+    
+            console.log("This item is already in the cart ")
+    
+              User.findOneAndUpdate(
+                    { _id: req.params.id, "Cart.id": req.body.itemID},
+                    { $inc: { "Cart.$.quantity": 1 } },
+                    { new: true },
+                    () => {
+                        if (err) return res.json({ success: false, err });
+                        res.status(200).json(userInfo.addToCart)
                     }
-
-                    },
-
-                {new: true },
-
-                 (err, userInfo) => {
-                     if (err) return res.json({ success: false, err });
-                     res.status(200).json(userInfo.Cart)
-                }    
+                )
+    
+           
+            } else {
+    
+                console.log("In the else clause")
                 
-            )
+                User.findOneAndUpdate(
+                    { _id: req.params.id },
+                    {
+                        $push: {
+                            Cart: {
+                                id: req.body.itemID,
+                                quantity: 1
+                            }
+                        }
     
+                        },
     
-  }
-
-})
-
-
-
+                    {new: true },
+    
+                     (err, userInfo) => {
+                         if (err) return res.json({ success: false, err });
+                         res.status(200).json(userInfo.Cart)
+                    }    
+                    
+                )
+        
+        
+      }
+    
+    })
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 //Display Cart Items
 
-router.route('/display/:id').get((req,res) => {
-
-    User.findById(req.params.id)
-    .then(user=> res.json(user))
-    .catch(err => res.status(400).json('Error : '  +err));
+router.route('/display/:id').get(async (req,res) => {
+    try {
+        User.findById(req.params.id)
+        .then(user=> res.json(user))
+        .catch(err => res.status(400).json('Error : '  +err));
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 
 //Delete items from the cart
 
-router.route('/delete/:id').post((req,res) => {
-
-    User.findOneAndUpdate(
-       { _id: req.params.id },
-       {
-           $pull: {
-            Cart: {
-                   id: req.body.itemID
-               }
-           }
-
-           },
-
-       {new: true },
-
-        (err, userInfo) => {
-            if (err) return res.json({ success: false, err });
-            res.status(200).json(userInfo.Cart)
-       }    
-       
-   )
+router.route('/delete/:id').post(async (req,res) => {
+    try {
+        User.findOneAndUpdate(
+            { _id: req.params.id },
+            {
+                $pull: {
+                 Cart: {
+                        id: req.body.itemID
+                    }
+                }
+     
+                },
+     
+            {new: true },
+     
+             (err, userInfo) => {
+                 if (err) return res.json({ success: false, err });
+                 res.status(200).json(userInfo.Cart)
+            }    
+            
+        )
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 
--- server/route/wishList.js
+++ server/route/wishList.js
@@ -4,95 +4,100 @@ const router = express.Router();
 const User = require('../models/user'); //user schema
 
 //Adding items to the WishList
-router.route('/add/:id').post((req,res) => {
-
-    console.log(req.params.id);
-    console.log(req.body.itemID);
-
-    User.findOne({ _id: req.params.id }, (err, userInfo) => {
-        let alreadyInWishList = false;
-
-        userInfo.WishList.forEach((item) => {
-
-        
-            if (item.id == req.body.itemID) {
-                alreadyInWishList = true;
+router.route('/add/:id').post(async (req,res) => {
+    try {
+         await User.findOne({ _id: req.params.id }, (err, userInfo) => {
+            let alreadyInWishList = false;
+    
+            userInfo.WishList.forEach((item) => {
+    
+            
+                if (item.id == req.body.itemID) {
+                    alreadyInWishList = true;
+                    
+                }
+            })
+    
+            if (alreadyInWishList) {
+    
+    
+            console.log("This item is already in the wish List ")
+           
+            } else {
+    
+                console.log("In the else clause")
                 
-            }
-        })
-
-        if (alreadyInWishList) {
-
-
-        console.log("This item is already in the wish List ")
-       
-        } else {
-
-            console.log("In the else clause")
-            
-            
-            
-            User.findOneAndUpdate(
-                { _id: req.params.id },
-                {
-                    $push: {
-                        WishList: {
-                            id: req.body.itemID
+                
+                
+                User.findOneAndUpdate(
+                    { _id: req.params.id },
+                    {
+                        $push: {
+                            WishList: {
+                                id: req.body.itemID
+                            }
                         }
-                    }
-
-                    },
-
-                {new: true },
-
-                 (err, userInfo) => {
-                     if (err) return res.json({ success: false, err });
-                     res.status(200).json(userInfo.WishList)
-                }    
-                
-            )
-        
-        
-
+    
+                        },
+    
+                    {new: true },
+    
+                     (err, userInfo) => {
+                         if (err) return res.json({ success: false, err });
+                         res.status(200).json(userInfo.WishList)
+                    }    
+                    
+                )
+            
             
     
-  }
-
-})
+                
+        
+      }
+    
+    })
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 //Display WishList
-router.route('/display/:id').get((req,res) => {
-
-     
-    User.findById(req.params.id)
-    .then(user=> res.json(user))
-    .catch(err => res.status(400).json('Error : '  +err));
+router.route('/display/:id').get(async (req,res) => {
+    try {
+        await User.findById(req.params.id)
+        .then(user=> res.json(user))
+        .catch(err => res.status(400).json('Error : '  +err));
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 //Delete Wishlist Items
 
-router.route('/delete/:id').post((req,res) => {
-
-     User.findOneAndUpdate(
-        { _id: req.params.id },
-        {
-            $pull: {
-                WishList: {
-                    id: req.body.itemID
+router.route('/delete/:id').post(async (req,res) => {
+    try {
+        await User.findOneAndUpdate(
+            { _id: req.params.id },
+            {
+                $pull: {
+                    WishList: {
+                        id: req.body.itemID
+                    }
                 }
-            }
-
-            },
-
-        {new: true },
-
-         (err, userInfo) => {
-             if (err) return res.json({ success: false, err });
-             res.status(200).json(userInfo. WishList)
-        }    
-        
-    )
+    
+                },
+    
+            {new: true },
+    
+             (err, userInfo) => {
+                 if (err) return res.json({ success: false, err });
+                 res.status(200).json(userInfo. WishList)
+            }    
+            
+        )
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 module.exports = router;
