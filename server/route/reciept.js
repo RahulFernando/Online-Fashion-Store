29f4c4bf28502cb5a5ab15b74526bb494efb5fa3
plhewa <plhewa2@gmail.com>
2020-05-10 13:12:34 +0530

    Payment Error fixed

--- server/route/reciept.js
+++ server/route/reciept.js
@@ -3,14 +3,12 @@ const router = express.Router();
 
 const {History} = require('../models/purchaseHistory'); //purchse History schema
 
-router.route('/displayReciept/:id').get(async(req,res) => {
-    try {           
-        await History.findById(req.params.id)
+router.route('/displayReciept/:id').get((req,res) => {
+
+    History.findById(req.params.id)
         .then(history => res.json(history))
         .catch(err => res.status(400).json('Error : '  +err));
-    } catch (error) {
-        console.log(error)
-    }
+
 });
 
 
